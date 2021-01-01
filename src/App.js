d517ea986c27c57da9c7aafa46cc1d320b300479
andrey-dum <duminskiy@gmail.com>
2021-01-01 17:11:57 +0200

    firebase. Hooks useTasks, useProjects

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@
 import './App.scss';
 import Content from './components/Layout/Content';
 import Header from './components/Layout/Header';
+import { useTasks } from './hooks';
 
 export const App = () => {
+  const { tasks } = useTasks();
+  console.log(tasks);
   return (
     <div className="app">
       <Header />
-
+    
       <Content />
     </div>
   );
