811ffbac8615d77f22e3ffb5b04cbdc250755bb8
andrey-dum <duminskiy@gmail.com>
2021-01-02 18:02:31 +0200

    Tasks component

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -48,19 +48,49 @@ export default function Sidebar() {
                     
                 </li>
                 <li
-                    data-testid="today"
-                    className={'today'}
+                   data-testid="today"
+                   className={active === 'today' ? 'active' : undefined}
                 >
-                    <div>
+                    <div
+                        data-testid="today-action"
+                        aria-label="Show today's tasks"
+                        tabIndex={0}
+                        role="button"
+                        onClick={() => {
+                        setActive('today');
+                        setSelectedProject('TODAY');
+                        }}
+                        onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            setActive('today');
+                            setSelectedProject('TODAY');
+                        }
+                        }}
+                    >
                         <span><FaRegCalendar /></span>
                         <span>Today</span>
                     </div> 
                 </li>
                 <li 
                     data-testid="next_7"
-                    className={'next_7'}
+                    className={active === 'next_7' ? 'active' : undefined}
                 >
-                    <div>
+                    <div
+                        data-testid="next_7-action"
+                        aria-label="Show tasks for the next 7 days"
+                        tabIndex={0}
+                        role="button"
+                        onClick={() => {
+                        setActive('next_7');
+                        setSelectedProject('NEXT_7');
+                        }}
+                        onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            setActive('next_7');
+                            setSelectedProject('NEXT_7');
+                        }
+                        }}
+                    >
                         <span>
                             <FaRegCalendarAlt />
                         </span>
@@ -80,9 +110,9 @@ export default function Sidebar() {
                 tabIndex={0}
             >
                 <span>
-                <FaChevronDown
-                    className={!showProjects ? 'hidden-projects' : undefined}
-                />
+                    <FaChevronDown
+                        className={!showProjects ? 'hidden-projects' : undefined}
+                    />
                 </span>
                 <h2>Projects</h2>
             </div>
